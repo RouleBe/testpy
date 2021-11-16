dae5011b95ee36b872ac83a58f36cca2a9894b21
roulbé <beroule.alexandre@gmail.com>
2021-11-16 05:29:38 +0100

    DeadLine
    to do exo test python

---
+++ testpyproject/users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_view
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('problem/', views.problem, name='problem'),
+    path('solve/',views.solve, name='solve'),
+    path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    
+]
